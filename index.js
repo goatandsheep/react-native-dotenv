5e9e3e54159e19ed03e12d8f150413ecd2fc4f5d
Bertrand Marron <bertrand.marron@gmail.com>
2017-10-25 22:36:12 +0200

    Use xo and fix code style

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-var dotEnv = require('dotenv');
-var fs = require('fs');
-var sysPath = require('path');
-var process = require('process');
+const {join} = require('path')
+const dotenv = require('dotenv')
 
 module.exports = function (data) {
-    var t = data.types;
+  const t = data.types
 
-    return {
-        visitor: {
-            ImportDeclaration: function(path, state) {
-                var options = state.opts;
+  return {
+    visitor: {
+      ImportDeclaration(path, state) {
+        const production = process.env.BABEL_ENV === 'production'
+        const options = state.opts
 
-                if (options.replacedModuleName === undefined)
-                  return;
-
-                var configDir = options.configDir ? options.configDir : './';
-                var configFile = options.filename ? options.filename : '.env';
-
-                if (path.node.source.value === options.replacedModuleName) {
-                  var config = dotEnv.config({ path: sysPath.join(configDir, configFile), silent: true }) || {};
-                  var platformPath = (process.env.BABEL_ENV === 'development' || process.env.BABEL_ENV === undefined)
-                                          ? configFile + '.development'
-                                          : configFile + '.production';
-                  var config = Object.assign(config, dotEnv.config({ path: sysPath.join(configDir, platformPath), silent: true }));
-
-                  path.node.specifiers.forEach(function(specifier, idx){
-                    if (specifier.type === "ImportDefaultSpecifier") {
-                      throw path.get('specifiers')[idx].buildCodeFrameError('Import dotenv as default is not supported.')
-                    }
-                    var importedId = specifier.imported.name
-                    var localId = specifier.local.name;
-
-                    if(!config[importedId]) {
-                      throw path.get('specifiers')[idx].buildCodeFrameError('Try to import dotenv variable "' + importedId + '" which is not defined in any ' + configFile + ' files.')
-                    }
-
-                    var binding = path.scope.getBinding(localId);
-                    binding.referencePaths.forEach(function(refPath){
-                      if (config[importedId]) {
-                        refPath.replaceWith(t.valueToNode(config[importedId]))
-                      }
-                    });
-                  })
-
-                  path.remove();
-                }
-            }
+        if (options.replacedModuleName === undefined) {
+          return
         }
+
+        const configDir = options.configDir ? options.configDir : './'
+        const configFile = options.filename ? options.filename : '.env'
+
+        if (path.node.source.value === options.replacedModuleName) {
+          let config = dotenv.config({
+            path: join(configDir, configFile),
+            silent: true}
+          ) || {}
+
+          const platformPath = production ? `${configFile}.production` : `${configFile}.development`
+
+          config = Object.assign(config, dotenv.config({
+            path: join(configDir, platformPath), silent: true
+          }))
+
+          path.node.specifiers.forEach((specifier, idx) => {
+            if (specifier.type === 'ImportDefaultSpecifier') {
+              throw path.get('specifiers')[idx].buildCodeFrameError('Import dotenv as default is not supported.')
+            }
+
+            const importedId = specifier.imported.name
+            const localId = specifier.local.name
+
+            if (!config[importedId]) {
+              throw path.get('specifiers')[idx].buildCodeFrameError(`Try to import dotenv variable "${importedId}" which is not defined in any ${configFile} files.'`)
+            }
+
+            const binding = path.scope.getBinding(localId)
+            binding.referencePaths.forEach(refPath => {
+              if (config[importedId]) {
+                refPath.replaceWith(t.valueToNode(config[importedId]))
+              }
+            })
+          })
+
+          path.remove()
+        }
+      }
     }
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,61 +1,52 @@
-var babel = require("babel-core");
-var expect = require('expect.js');
-var process = require('process');
+const babel = require('babel-core')
+const expect = require('expect.js')
 
-var createPluginsWithConfigDir = function(configDir) {
-  return ['babel-plugin-transform-es2015-modules-commonjs', ['../../../',
-  {
-    replacedModuleName: 'babel-dotenv',
-    configDir: configDir,
-  }]];
-}
-
-describe('myself in some tests', function() {
-  it('should throw if variable not exist', function() {
-    expect(function(){
+describe('myself in some tests', () => {
+  it('should throw if variable not exist', () => {
+    expect(() => {
       babel.transformFileSync('test/fixtures/variable-not-exist/source.js')
-    }).to.throwException(function (e) {
-      expect(e.message).to.contain("Try to import dotenv variable \"foo\" which is not defined in any .env files.");
-    });
-  });
+    }).to.throwException(e => {
+      expect(e.message).to.contain('Try to import dotenv variable "foo" which is not defined in any .env files.')
+    })
+  })
 
-  it('should throw if default is imported', function() {
-    expect(function(){
+  it('should throw if default is imported', () => {
+    expect(() => {
       babel.transformFileSync('test/fixtures/default-imported/source.js')
-    }).to.throwException(function (e) {
-      expect(e.message).to.contain("Import dotenv as default is not supported.");
-    });
-  });
+    }).to.throwException(e => {
+      expect(e.message).to.contain('Import dotenv as default is not supported.')
+    })
+  })
 
-  it('should load default env from .env', function(){
-    var result = babel.transformFileSync('test/fixtures/default/source.js')
+  it('should load default env from .env', () => {
+    const result = babel.transformFileSync('test/fixtures/default/source.js')
     expect(result.code).to.be('\'use strict\';\n\nconsole.log(\'abc123\');\nconsole.log(\'username\');')
   })
 
-  it('should load let .env.development overwrite .env', function(){
-    var result = babel.transformFileSync('test/fixtures/dev-env/source.js')
+  it('should load let .env.development overwrite .env', () => {
+    const result = babel.transformFileSync('test/fixtures/dev-env/source.js')
     expect(result.code).to.be('\'use strict\';\n\nconsole.log(\'abc123\');\nconsole.log(\'userdonthavename\');')
   })
 
-  it('should load custom env file "build.env" and its overwrites', function(){
-    var result = babel.transformFileSync('test/fixtures/filename/source.js')
+  it('should load custom env file "build.env" and its overwrites', () => {
+    const result = babel.transformFileSync('test/fixtures/filename/source.js')
     expect(result.code).to.be('\'use strict\';\n\nconsole.log(\'abc123456\');\nconsole.log(\'userdonthavename123456\');')
   })
 
-  it('should load let .env.production overwrite .env', function(){
-    process.env['BABEL_ENV'] = 'production';
-    var result = babel.transformFileSync('test/fixtures/prod-env/source.js')
+  it('should load let .env.production overwrite .env', () => {
+    process.env.BABEL_ENV = 'production'
+    const result = babel.transformFileSync('test/fixtures/prod-env/source.js')
     expect(result.code).to.be('\'use strict\';\n\nconsole.log(\'abc123\');\nconsole.log(\'foobar\');')
-    process.env['BABEL_ENV'] = undefined;
+    process.env.BABEL_ENV = undefined
   })
 
-  it('should support `as alias` import syntax', function(){
-    var result = babel.transformFileSync('test/fixtures/as-alias/source.js')
+  it('should support `as alias` import syntax', () => {
+    const result = babel.transformFileSync('test/fixtures/as-alias/source.js')
     expect(result.code).to.be('\'use strict\';\n\nvar a = \'abc123\';\nvar b = \'username\';')
   })
 
-  it('should do nothing if no `replacedModuleName` provided', function(){
-    var result = babel.transformFileSync('test/fixtures/replaced-module-name-not-provided/source.js')
+  it('should do nothing if no `replacedModuleName` provided', () => {
+    const result = babel.transformFileSync('test/fixtures/replaced-module-name-not-provided/source.js')
     expect(result.code).to.be('\'use strict\';\n\nvar _fancyDotenv = require(\'fancy-dotenv\');\n\nvar _fancyDotenv2 = _interopRequireDefault(_fancyDotenv);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }')
   })
-});
+})
