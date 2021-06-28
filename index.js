a9b57bea1205d4939af2b694149effa905613ede
Kemal Ahmed <goatandsheep@gmail.com>
2021-06-28 14:04:19 -0400

    lint fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {readFileSync} = require('fs')
-const {resolve} = require('path')
+const {resolve} = require('node:path')
 const dotenv = require('dotenv')
 
 function parseDotenvFile(path, verbose = false) {
